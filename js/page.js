992b05cafd28c22ce3d4afc9e455e15495e0e87d
Noura Arafa <Noura Arafa>
2018-04-07 21:49:42 +0200

    Nouraaaaa

---
+++ js/page.js
@@ -0,0 +1,23 @@
+var slideIndex=1;
+document.getElementById("abouthidden").style.display='none'
+document.getElementById("serviceHidden").style.display='none'
+document.innerHTML=""
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function showSlides(n) {
+    var i;
+  var slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+     for (i = 0; i < slides.length; i++) {
+         //bay5fy kol aly ablyh 
+      slides[i].style.display = "none"; 
+  }
+    
+    //dah aly byzhrny 
+     slides[slideIndex-1].style.display = "block"; 
+}
+function gotoStyle(){
+    document.getElementById("whiteSlider").style.backgroundColor ='white'
+}
