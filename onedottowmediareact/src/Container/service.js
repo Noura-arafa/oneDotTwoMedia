5fb93cc3c4241416113a21de0becbfbba7a5eaf9
khadegaosman <khadiha.osman16@gmail.com>
2018-05-02 13:12:37 +0200

    component

---
+++ onedottowmediareact/src/Container/service.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import classes from '../../css/service-contact-style'
+
+
