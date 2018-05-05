3e6732c62e4111a052a0a1e5ee457804a4e4e1b7
Marwa Saied <Marwa Saied>
2018-05-05 21:03:21 +0200

    routing

--- onedottowmediareact/src/App.js
+++ onedottowmediareact/src/App.js
@@ -7,6 +7,7 @@ import DotDevServices from './Container/DotDevServices.js'
 import Page404 from './Container/Page404.js'
 import DotMediaServices from './Container/DotMediaServices.js'
 import {BrowserRouter as Router,Route} from 'react-router-dom'
+import Navbar from './Container/navbar';
 import './App.css';
 
 
@@ -14,11 +15,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-
+    	
+       
       <Router>
       	<div>
+      	<Navbar/>
       		<Route exact path="/404"render={() => <div className="App"> <Page404/> </div>}/>
-      		<Route exact path="/DotAcademy"render={() => <div className="App"> <Nav/>  <DotAcademy/> <Footer/> </div>}/>
+      		<Route exact path="/DotAcademy"render={() => <div className="App">  
+      		 <DotAcademy/>  </div>}/>
       		<Route exact path="/DotAnalytics"render={() => <div className="App"> <DotAnalytics/> </div>}/>
       		<Route exact path="/DotDevServices"render={() => <div className="App"> <DotDevServices/> </div>}/>
       		<Route exact path="/DotMediaServices"render={() => <div className="App"> <DotMediaServices/> </div>}/>
---
+++ onedottowmediareact/src/Container/navbar.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../css/footer-nav-style.css'
+import logo from '../images/logo.png'
+
+
+class Navbar extends Component {
+	state = {
+
+		clicked  : false,
+	}
+	show =e =>{
+	// 	if(this.state.clicked = false){
+	// 	document.getElementById("dropdown_con").style.display="block";
+	// }
+
+          
+	};
+  render() {
+    return (
+
+    	<div className="nav-bar">
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col-md-6">
+					<img src={logo}/>
+				</div>
+					<div className="col-md-6 text-right">
+						<button class="search_btn"><i class="fa fa-search"></i></button>
+						<div className ="dropdown">
+						<button onClick ={e =>this.show(e)} className="bar_btn"><i className="fa fa-bars"></i>
+						<div id="dropdown_con" className="dropdown-content">
+					    <a href="/DotAcademy">DotAcademy</a>
+					    <a href="/DotAnalytics">DotAnalytics</a>
+					    <a href="/DotDevServices">DotDevServices</a>
+					    <a href="/DotMediaServices">DotMediaService</a>
+					    <a href="/DotPress">DotPress</a>
+					    <a href="/DotVid">DotVid</a>
+					  </div>
+						</button>
+						</div>
+
+					</div>
+			</div>
+		</div>
+	</div>
+
+    );
+  }
+}
+
+export default Navbar;
